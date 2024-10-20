4ec6aac6ef7868a77fbc5f20572a0e88793969d0
Suryakanta Prusty <kantaprustys@gmail.com>
2024-10-20 17:04:51 +0530

    added the getMessages route and controller

--- backend/middleware/protect.middleware.js
+++ backend/middleware/protect.middleware.js
@@ -3,7 +3,6 @@ import User from "../model/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    console.log(req.cookies);
     const token = req.cookies.jwt;
 
     if (!token) {
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { sendMessage } from "../controller/message.controller.js";
+import { sendMessage, getMessages } from "../controller/message.controller.js";
 import { protectRoute } from "../middleware/protect.middleware.js";
 
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
